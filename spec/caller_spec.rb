0d3a4ff46d4aa26565de7c82d0cbab5ba667ceaf
Kirill Shevchenko <kirills167@gmail.com>
2018-10-27 16:57:45 +0300

    add caller_for test

--- spec/caller_spec.rb
+++ spec/caller_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Caller do
   it 'should initialize instance and pass arguments to it' do
     expect(Sample.call('text')).to eq('text')
   end
+
+  describe 'caller_for' do
+    it 'should initialize instance with custom callable method' do
+      expect(SampleCallerFor.define('text')).to eq('text')
+    end
+  end
 end
